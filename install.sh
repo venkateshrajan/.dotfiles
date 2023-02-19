c8af906514558fc8c48e742958225f7954a13be1
venkateshrajan <venkatesh.xaviers@gmail.com>
2023-02-19 10:27:38 +0530

    added my oh-my-zsh config

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
 # Alacritty
 rm -rf ~/.config/alacritty
@@ -15,3 +15,8 @@ ln -s "$(pwd)/i3" ~/.config/i3
 # nvim
 rm -rf ~/.config/nvim
 ln -s "$(pwd)/init.lua/unix" ~/.config/nvim
+
+# ohmyzsh
+rm -rf ~/.zshrc
+ln -s "$(pwd)/.zshrc" ~/.zshrc
+source ~/.zshrc
