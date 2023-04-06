85c1bf6c65766dd4096c811cc144a8dce6ce3e3e
venkateshrajan <venkatesh.xaviers@gmail.com>
2023-04-06 18:41:11 +0530

    not adding the / before vaultcx

--- scripts/get-diff-names.sh
+++ scripts/get-diff-names.sh
@@ -1,1 +1,1 @@
-git diff HEAD~1 --name-only | awk -F\, '{cmd="pwd | grep -Po /vaultcx.*$"; cmd | getline mypwd; close(cmd); print mypwd "/" $0}'
+git diff HEAD~1 --name-only | awk -F\, '{cmd="pwd | grep -Po vaultcx.*$"; cmd | getline mypwd; close(cmd); print mypwd "/" $0}'
