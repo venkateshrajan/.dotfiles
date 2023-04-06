49aa926a643e3f5f87e75be6280a480faaf3c64f
venkateshrajan <venkatesh.xaviers@gmail.com>
2023-04-06 16:19:35 +0530

    added -u option for tmux selector

--- scripts/tmux-session-finder.sh
+++ scripts/tmux-session-finder.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 name=$(tmux ls -F '#{session_attached},#{session_activity},#{session_name}' | sort -r | sed '/^$/d' | cut -d',' -f3- | fzf --reverse --header jump-to-session --preview 'tmux capture-pane -pt {}')
-tmux a -t "$name"
+tmux -u a -t "$name"
