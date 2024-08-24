56a816496d5b5472616b8b6985f68a957f0ff0ae
venkateshrajan <venkatesh.xaviers@gmail.com>
2024-08-24 17:30:21 +0530

    Added rocky installation commands

--- install/nvim.sh
+++ install/nvim.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 # Utility functions
-check_if_installed() {
+check_if_installed_ubuntu() {
   dpkg -l $1 | grep $1 | awk '{ print $2 }' | wc -l
+}
+
+check_if_installed_rocky() {
+  rpm -qa | grep -w $1 | aws 'BEGIN {FS="-"} {print $1}' | wc -l
 }
 
 get_os_id() {
@@ -16,7 +20,7 @@ install_ubuntu() {
   declare -a pkgs_not_available=()
   for pkg in "${required_packages[@]}"
   do
-    if [ `check_if_installed "$pkg" 2> /dev/null` != 1 ] 
+    if [ `check_if_installed_ubuntu "$pkg" 2> /dev/null` != 0 ] 
     then
       pkgs_not_available+=("$pkg")
     fi
@@ -35,7 +39,27 @@ install_ubuntu() {
 }
 
 install_rocky() {
-  echo "Coming soon"
+  # Check if required packages are installed.
+  declare -a required_packages=("sudo" "curl")
+  declare -a pkgs_not_available=()
+  for pkg in "${required_packages[@]}"
+  do
+    if [ `check_if_installed_rocky "$pkg" 2> /dev/null` == 0 ] 
+    then
+      pkgs_not_available+=("$pkg")
+    fi
+  done
+
+  # Install the packages which are not installed already
+  yum install "${pkgs_not_available[@]}"
+
+  # Refer https://github.com/neovim/neovim/blob/master/INSTALL.md
+  curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+  sudo rm -rf /opt/nvim
+  sudo tar -C /opt -xzf nvim-linux64.tar.gz
+
+  echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >> ~/.bashrc
+  echo "Please run 'source ~/.bashrc'"
 }
 
 # Check the OS type
