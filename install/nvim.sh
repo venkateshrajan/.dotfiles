3ce3022e8a80f29217deb97c67a87bd3fa968bc7
venkateshrajan <venkatesh.xaviers@gmail.com>
2024-08-24 11:30:23 +0530

    Added package checks for nvim installation

--- install/nvim.sh
+++ install/nvim.sh
@@ -1,6 +1,30 @@
 #! /bin/bash
 
-# Refer https://github.com/neovim/neovim/blob/master/INSTALL.md
+# utility functions
+check_if_installed() {
+  dpkg -l $1 | grep $1 | awk '{ print $2 }' | wc -l
+}
+
+
+# check if required packages are installed.
+
+# sudo
+if [ `check_if_installed "sudo" 2> /dev/null` != 1 ] 
+then
+  echo "Installing sudo";
+  apt install sudo
+fi
+
+# curl
+if [ `check_if_installed "curl" 2> /dev/null` != 1 ] 
+then
+  echo "Installing curl";
+  sudo apt install curl
+fi
+
+
+Refer https://github.com/neovim/neovim/blob/master/INSTALL.md
+
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
