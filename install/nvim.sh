ac0d9a5bdc50ab6e84bbdd6720a45e4354e6046c
venkateshrajan <venkatesh.xaviers@gmail.com>
2024-08-26 14:19:58 +0530

    Fixed provider installation commands

--- install/nvim.sh
+++ install/nvim.sh
@@ -17,7 +17,7 @@ get_os_id() {
 install_ubuntu() {
   # Check if required packages are installed.
   declare -a required_packages=("sudo" "curl" "ripgrep" 
-      "yarn" "npm" "perl" "python3" "ruby" "gem")
+      "yarn" "npm" "perl" "python3" "ruby-dev" "gem")
   declare -a pkgs_not_available=()
   for pkg in "${required_packages[@]}"
   do
@@ -28,7 +28,7 @@ install_ubuntu() {
   done
 
   # Install the packages which are not installed already
-  apt install "${pkgs_not_available[@]}"
+  apt install "${pkgs_not_available[@]}" -y
 
   # Refer https://github.com/neovim/neovim/blob/master/INSTALL.md
   curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
@@ -36,10 +36,12 @@ install_ubuntu() {
   sudo tar -C /opt -xzf nvim-linux64.tar.gz
 
   # Install providers
+  python3 -m venv ~/.venky
+  source ~/.venky/bin/activate
+  python3 -m pip install --user --upgrade pynvim
   npm install -g neovim
   gem install neovim
-  python3 -m pip install --user --upgrade pynvim
-  cpanm -n Neovim:Ext
+  cpan -i Neovim:Ext
 
   echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >> ~/.bashrc
   echo "Please run 'source ~/.bashrc'"
