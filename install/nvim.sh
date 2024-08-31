127fe1c2c7d50794d7e58d35c40c5183efc9f72f
venkateshrajan <venkatesh.xaviers@gmail.com>
2024-08-31 23:15:07 +0530

    Fixing an if condition

--- install/nvim.sh
+++ install/nvim.sh
@@ -33,7 +33,7 @@ nvim_download() {
   # Refer https://github.com/neovim/neovim/blob/master/INSTALL.md
   local nvim_path="/opt/nvim-linux64/bin"
   local download_url="https://github.com/neovim/neovim/releases/download/stable/nvim.appimage"
-  if "$2" -ne "0" ]; then 
+  if [["$2" -ne "0" ]]; then 
     download_url="https://github.com/neovim/neovim/releases/download/v0.9.5/nvim.appimage"
   fi
 
