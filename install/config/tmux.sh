e297c3481f7bdfaa554c18891c78850e32083579
venkateshrajan <venkatesh.xaviers@gmail.com>
2024-09-08 00:15:42 +0530

    More tmux configuration

--- install/config/tmux.sh
+++ install/config/tmux.sh
@@ -15,4 +15,5 @@ if [ -f "`echo ~`/.tmux-venky.conf" ]; then
 fi
 
 ln -s -r "$config_dir/../../.tmux.conf" "`echo ~`/.tmux-venky.conf"
+ln -s -r "$config_dir/../../.tmux.conf" "`echo ~`/.tmux.conf"
 echo -e "${LGREEN}Please run:${NC} ${BLUE}echo 'alias vtmux=\"tmux -L venky -f \x24HOME/.tmux-venky.conf\"' >> \x24HOME/.bashrc && source \x24HOME/.bashrc${NC}"
